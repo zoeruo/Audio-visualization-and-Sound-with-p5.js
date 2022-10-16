4f564e72a5a37b4562376d3881b85fc8bd162fdc
zoeruo <dodoruo@gmail.com>
2022-10-16 00:35:03 -0400

    basic waves

--- sketch.js
+++ sketch.js
@@ -1,9 +1,38 @@
-function setup(){
+let yoff = 0.0; // 2nd dimension of perlin noise
 
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(0,37,106);
 }
 
+function draw() {
+  //background(0,37,106);
 
+  fill(255);
+  // We are going to draw a polygon out of the wave points
+  beginShape();
 
-function draw(){
-    
+  let xoff = 0; // Option #1: 2D Noise
+  // let xoff = yoff; // Option #2: 1D Noise
+
+  // Iterate over horizontal pixels
+  for (let x = 0; x <= width; x += 10) {
+    // Calculate a y value according to noise, map to
+
+    // Option #1: 2D Noise
+    let y = map(noise(xoff, yoff), 0, 1, 200, 300);
+
+    // Option #2: 1D Noise
+    // let y = map(noise(xoff), 0, 1, 200,300);
+
+    // Set the vertex
+    vertex(x, y);
+    // Increment x dimension for noise
+    xoff += 0.05;
+  }
+  // increment y dimension for noise
+  yoff += 0.01;
+  vertex(width, height);
+  vertex(0, height);
+  endShape(CLOSE);
 }
